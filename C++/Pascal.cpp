3e28452f8f88c78ce5018c187a637fd42a6697cd
aparnasathyan01 <54775197+aparnasathyan01@users.noreply.github.com>
2019-10-08 22:55:13 +0530

    Add files via upload

---
+++ C++/Pascal.cpp
@@ -0,0 +1,27 @@
+
+#this is my favourite code because the triangle is simple yet very mathematically rich
+
+#include <iostream>
+#include <conio.h>
+
+void main()
+{
+    int rows, coef = 1;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    for(int i = 0; i < srows; i++)
+    {
+        for(int space = 1; space <= rows-i; space++)
+            cout <<"  ";
+        for(int j = 0; j <= i; j++)
+        {
+            if (j == 0 || i == 0)
+                coef = 1;
+            else
+                coef = coef*(i-j+1)/j;
+            cout << coef << "   ";
+        }
+        cout << endl;
+    }
+    getch();
+}
